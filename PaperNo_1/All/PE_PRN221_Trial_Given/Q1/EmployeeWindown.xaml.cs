8b8356845383e621fdedcb4eaa71dfe03624d604
tungb12ok <99113840+tungb12ok@users.noreply.github.com>
2024-03-17 08:42:00 +0700

    Update

---
+++ PaperNo_1/All/PE_PRN221_Trial_Given/Q1/EmployeeWindown.xaml.cs
@@ -0,0 +1,136 @@
+﻿using Q1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Q1
+{
+    /// <summary>
+    /// Interaction logic for EmployeeWindown.xaml
+    /// </summary>
+    public partial class EmployeeWindown : Window
+    {
+        PRN221_TrialContext context = new PRN221_TrialContext();
+        public EmployeeWindown()
+        {
+            InitializeComponent();
+            getData();
+        }
+        public void getData()
+        {
+            var list = context.Employees.ToList();
+            dgvEmployee.ItemsSource = list;
+        }
+        private void rbMale_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            txtEmployeeId.Text = string.Empty;
+            txtEmployeeName.Text = string.Empty;
+            txtIdNumber.Text = string.Empty;
+            txtPhone.Text = string.Empty;
+            txtDOB.Text = string.Empty;
+            rbMale.IsChecked = true;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Employee em = new Employee()
+            {
+                Name = txtEmployeeName.Text,
+                Gender = rbMale.IsChecked == true ? "Male" : "Female",
+                Dob = DateTime.Parse(txtDOB.Text),
+                Phone = txtPhone.Text,
+                Idnumber = txtIdNumber.Text
+            };
+
+            try
+            {
+                context.Employees.Add(em);
+                context.SaveChanges();
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            getData();
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Employee em = new Employee()
+            {
+                Id = int.Parse(txtEmployeeId.Text),
+                Name = txtEmployeeName.Text,
+                Gender = rbMale.IsChecked == true ? "Male" : "Female",
+                Dob = DateTime.Parse(txtDOB.Text),
+                Phone = txtPhone.Text,
+                Idnumber = txtIdNumber.Text
+            };
+
+            Employee update = context.Employees.FirstOrDefault(x => x.Id == em.Id);
+            update.Id = em.Id;
+            update.Name = em.Name;
+            update.Gender = em.Gender.ToString();
+            update.Phone = em.Phone.ToString();
+            update.Idnumber = em.Idnumber;
+
+            try
+            {
+                context.Employees.Update(em);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            getData();
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+            Employee remove = context.Employees.Where(x => x.Id == int.Parse(txtEmployeeId.Text)).FirstOrDefault();
+
+            try
+            {
+                context.Employees.Remove(remove);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            getData();
+        }
+
+        private void dgvEmployee_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            Employee selectedRow = (Employee)dgvEmployee.SelectedItem;
+            if (selectedRow != null)
+            {
+               txtEmployeeId.Text = selectedRow.Id.ToString();
+                txtEmployeeName.Text = selectedRow.Name.ToString();
+                txtIdNumber.Text = selectedRow.Idnumber.ToString();
+                txtPhone.Text = selectedRow.Phone.ToString();
+                txtDOB.Text = selectedRow.Dob.ToString();
+                rbMale.IsChecked = selectedRow.Gender.Equals("Male") ? true : false; ;
+                rbFemale.IsChecked = selectedRow.Gender.Equals("Female") ? true : false;
+            }
+        }
+    }
+}
---
+++ PaperNo_1/All/PE_PRN221_Trial_Given/Q2/Pages/List.cshtml.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Q2.Models;
+using Q2.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Q2.Pages
+{
+    public class ListModel : PageModel
+    {
+        private readonly PRN221_TrialContext _context;
+
+        public ListModel(PRN221_TrialContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public IFormFile FileInput { get; set; }
+
+        [BindProperty]
+        public string Search { get; set; }
+
+        public List<Service> ListService { get; set; }
+        public XmlDataModel XmlData { get; set; }
+
+
+        public IActionResult OnGet(string search)
+        {
+            int year = 2022;
+            int month = 3;
+
+            ListService = _context.Services
+                .Include(x => x.EmployeeNavigation)
+                .Where(x => x.Year == year && x.Month == month)
+                .ToList();
+            Search = search;
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                ListService = ListService.Where(x => x.RoomTitle == search).ToList();
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (FileInput == null || FileInput.Length <= 0)
+            {
+                return RedirectToPage("List");
+            }
+
+            using (var memoryStream = new MemoryStream())
+            {
+                FileInput.CopyTo(memoryStream);
+                memoryStream.Position = 0;
+
+                var serializer = new XmlSerializer(typeof(XmlDataModel));
+                XmlData = (XmlDataModel)serializer.Deserialize(memoryStream);
+
+                try
+                {
+                    if (XmlData != null)
+                    {
+                        foreach (var item in XmlData.Services)
+                        {
+                            Service s = new Service()
+                            {
+                                Amount = item.Amount,
+                                FeeType = item.FeeType,
+                                Id = item.Id,
+                                RoomTitle = item.RoomTitle,
+                                Month = (byte)item.Month,
+                                Year = item.Year,
+                                PaymentDate = DateTime.Parse(item.PaymentDate),
+                                Employee = int.Parse(item.Employee)
+
+                            };
+                            _context.Add(item);
+                            _context.SaveChanges();
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return RedirectToPage("List");
+        }
+    }
+    [XmlRoot("ArrayOfService")]
+    public class XmlDataModel
+    {
+        [XmlElement("Service")]
+        public ServiceModel[] Services { get; set; }
+    }
+
+    public class ServiceModel
+    {
+        public int Id { get; set; }
+        public string RoomTitle { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public string FeeType { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentDate { get; set; }
+        public string Employee { get; set; }
+    }
+}
